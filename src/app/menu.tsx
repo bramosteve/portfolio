8c66494fa70f871e070bf914bf1749e454fcf1b2
bramosteve <joclaverbrams@gmail.com>
2025-03-20 11:48:49 +0100

    troisieme commit

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -5,25 +5,35 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white text-black">
       {/* Navbar */}
-      <nav className="flex justify-between items-center p-4 border-b">
-        <div className="flex space-x-6">
-          <a href="#" className="underline">Shop</a>
-          <a href="#">Lookbook</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </div>
-        <h1 className="text-xl font-semibold">Alameda</h1>
-        <div className="flex space-x-4 items-center">
-          <FaInstagram />
-          <FaTwitter />
-          <div className="relative">
-            <FiShoppingCart className="text-xl" />
-            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-1 rounded-full">0</span>
-          </div>
-        </div>
-      </nav>
+      <div className="flex justify-between items-center p-4 border-b">
+           
+
+                    <div className="flex space-x-6">
+                        <a href="#" className="underline">Shop</a>
+                        <a href="#">Lookbook</a>
+                        <a href="#">About</a>
+                        <a href="#">Contact</a>
+                    </div>
+
+                    <div>
+                        <h1 className="text-xl font-semibold">Alameda</h1>
+                    </div>
+
+
+                    <div className="flex space-x-4 items-center">
+                        <FaInstagram />
+                        <FaTwitter />
+                        <div className="relative">
+                            <FiShoppingCart className="text-xl" />
+                            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-1 rounded-full">0</span>
+                        </div>
+                    </div>
+
+           
+      </div>
 
       {/* Categories */}
+
       <div className="text-center py-6 space-x-6">
         <a href="#" className="text-gray-700">Tops</a>
         <span className="text-gray-400">|</span>
