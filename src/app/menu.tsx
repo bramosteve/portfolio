337cbb030e0428bc9712725edeb051900b12d907
bramosteve <joclaverbrams@gmail.com>
2025-03-20 12:08:39 +0100

    troisieme commit

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -3,47 +3,40 @@ import { FiShoppingCart } from 'react-icons/fi';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-black">
-      {/* Navbar */}
-      <div className="flex justify-between items-center p-4 border-b">
-           
+    <div className="min-h-screen bg-white text-black flex flex-col items-center">
+    {/* Navbar */}
+            <div className="w-full border-b p-4 flex justify-between items-center max-w-6xl">
+                <div className="flex space-x-6">
+                    <a href="#" className="underline">Shop</a>
+                    <a href="#">Lookbook</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                </div>
+                <h1 className="text-xl font-semibold">Alameda</h1>
+                <div className="flex space-x-4 items-center">
+                    <FaInstagram />
+                    <FaTwitter />
+                    <div className="relative">
+                    <FiShoppingCart className="text-xl" />
+                    <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-1 rounded-full">0</span>
+                    </div>
+                </div>
+                </div>
 
-                    <div className="flex space-x-6">
-                        <a href="#" className="underline">Shop</a>
-                        <a href="#">Lookbook</a>
-                        <a href="#">About</a>
-                        <a href="#">Contact</a>
+                    {/* Titre principal */}
+                    <div className="text-center py-6">
+                        <h2 className="text-2xl font-bold">Alameda</h2>
                     </div>
 
-                    <div>
-                        <h1 className="text-xl font-semibold">Alameda</h1>
-                    </div>
-
-
-                    <div className="flex space-x-4 items-center">
-                        <FaInstagram />
-                        <FaTwitter />
-                        <div className="relative">
-                            <FiShoppingCart className="text-xl" />
-                            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-1 rounded-full">0</span>
-                        </div>
-                    </div>
-
-           
-      </div>
-
-      {/* Categories */}
-
-      <div className="text-center py-6 space-x-6">
-        <a href="#" className="text-gray-700">Tops</a>
-        <span className="text-gray-400">|</span>
-        <a href="#" className="text-gray-700">Bottoms</a>
-        <span className="text-gray-400">|</span>
-        <a href="#" className="text-gray-700">Sale</a>
-      </div>
-
-
+                {/* Catégories */}
+                <div className="text-center py-4 space-x-6">
+                    <a href="#" className="text-gray-700">Hauts</a>
+                    <span className="text-gray-400">|</span>
+                    <a href="#" className="text-gray-700">Bas</a>
+                    <span className="text-gray-400">|</span>
+                    <a href="#" className="text-gray-700">Soldes</a>
+                </div>
     </div>
-  );
+  )
 }
 
